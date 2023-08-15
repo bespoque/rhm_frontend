0671db3d4ea25ccb24f6e15ca0a10d7802400163
prinxard <prinxard@gmail.com>
2023-08-15 08:14:33 +0100

    update

--- src/components/tables/viewPayeTccTablePrint.js
+++ src/components/tables/viewPayeTccTablePrint.js
@@ -262,7 +262,7 @@ export const ViewSinglePayeTccPrintTable = ({
               </div>
               <div className="flex justify-center">
                 <div>
-                  <h6 className="text-green-600">KOGI STATE GOVERNMENT</h6>
+                  <h2 className="text-green-600">KOGI STATE GOVERNMENT</h2>
                   <div className="text-center">
                     <p className="text-red-600 text-lg">TAX CLEARANCE CERTIFICATE</p>
                   </div>
