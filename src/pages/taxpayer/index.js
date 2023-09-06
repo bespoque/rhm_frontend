d853ad41c517342ba99849ff6361213471761438
prinxard <prinxard@gmail.com>
2023-09-06 08:30:28 +0100

    updated create individual

--- src/pages/taxpayer/index.js
+++ src/pages/taxpayer/index.js
@@ -51,7 +51,6 @@ export default function Index() {
     }, []);
 
     const onSubmit = (data) => {
-        console.log(data);
         setIsFetching(true)
         axios.post(`${url.BASE_URL}taxpayer/new-individual`, data)
             .then(function (response) {
@@ -100,7 +99,8 @@ export default function Index() {
                     <div className="grid grid-cols-3 gap-4">
                         <div className="form-group ">
                             <p>Title <span className="text-red-400">*</span></p>
-                            <select name="indv_title" ref={register()} className="form-control SlectBox mb-4 w-full rounded font-light text-gray-500">
+                            <select name="indv_title" required ref={register()} className="form-control SlectBox mb-4 w-full rounded font-light text-gray-500">
+                                <option value="">Please Select</option>
                                 <option value="Mr">Mr</option>
                                 <option value="Mrs">Mrs</option>
                                 <option value="Mrss">Miss</option>
@@ -109,14 +109,14 @@ export default function Index() {
 
                         <div className="form-group">
                             <p>Surname <span className="text-red-400">*</span></p>
-                            <input name="surname" type="text" className="form-control mb-4 w-full rounded font-light text-gray-500" ref={register({ required: "Surname is required" })}
+                            <input name="surname" required type="text" className="form-control mb-4 w-full rounded font-light text-gray-500" ref={register({ required: "Surname is required" })}
                             />
                             {errors.surname && <small className="text-red-600">{errors.surname.message}</small>}
                         </div>
 
                         <div className="form-group ">
                             <p>First Name <span className="text-red-400">*</span></p>
-                            <input name="first_name" ref={register({ required: "First name is required" })} type="text" className="form-control mb-4 w-full rounded font-light text-gray-500"
+                            <input name="first_name" required ref={register({ required: "First name is required" })} type="text" className="form-control mb-4 w-full rounded font-light text-gray-500"
                             />
                             {errors.first_name && <small className="text-red-600">{errors.first_name.message}</small>}
                         </div>
@@ -129,7 +129,7 @@ export default function Index() {
 
                         <div className="form-group ">
                             <p>Date of Birth <span className="text-red-400">*</span></p>
-                            <input name="birth_date" ref={register({ required: "Birthdate is required" })} type="date" className="form-control mb-4 w-full rounded font-light text-gray-500"
+                            <input name="birth_date" required ref={register({ required: "Birthdate is required" })} type="date" className="form-control mb-4 w-full rounded font-light text-gray-500"
                             />
                             {errors.birth_date && <small className="text-red-600">{errors.birth_date.message}</small>}
                         </div>
@@ -137,14 +137,15 @@ export default function Index() {
 
                         <div className="form-group ">
                             <p>Phone Number <span className="text-red-400">*</span></p>
-                            <input name="phone_number" ref={register({ required: "Phone number is Required" })} type="text" className="form-control mb-4 w-full rounded font-light text-gray-500"
+                            <input name="phone_number" required ref={register({ required: "Phone number is Required" })} type="text" className="form-control mb-4 w-full rounded font-light text-gray-500"
                             />
                             {errors.phone_number && <small className="text-red-600">{errors.phone_number.message}</small>}
                         </div>
 
                         <div className="form-group ">
                             <p>Gender <span className="text-red-400">*</span></p>
-                            <select name="gender" ref={register({ required: "Gender is Required" })} className="form-control SlectBox mb-4 w-full rounded font-light text-gray-500">
+                            <select name="gender" required ref={register({ required: "Gender is Required" })} className="form-control SlectBox mb-4 w-full rounded font-light text-gray-500">
+                                <option value="">Please Select</option>
                                 <option value="Female">Female</option>
                                 <option value="Male">Male</option>
                             </select>
@@ -153,21 +154,23 @@ export default function Index() {
 
                         <div className="form-group ">
                             <p>Marital Status <span className="text-red-400">*</span></p>
-                            <select name="marital_status" ref={register()} className="form-control SlectBox mb-4 w-full rounded font-light text-gray-500">
+                            <select name="marital_status" required ref={register()} className="form-control SlectBox mb-4 w-full rounded font-light text-gray-500">
+                                <option value="">Please Select</option>
                                 <option value="Single">Single</option>
                                 <option value="Married">Married</option>
                             </select>
                         </div>
 
                         <div className="form-group ">
-                            <p>State of residence <span className="text-red-400">*</span></p>
+                            <p>State of residence</p>
                             <input readOnly name="state_of_residence" value="Kogi" ref={register()} type="text" className="form-control mb-4 w-full rounded font-light text-gray-500"
                             />
                         </div>
 
                         <div className="form-group ">
-                            <p>LGA</p>
-                            <select name="lga" ref={register({ required: "LGA is Required" })} className="form-control SlectBox mb-4 w-full rounded font-light text-gray-500">
+                            <p>LGA <span className="text-red-400">*</span></p>
+                            <select name="lga" required ref={register({ required: "LGA is Required" })} className="form-control SlectBox mb-4 w-full rounded font-light text-gray-500">
+                                <option value="">Please Select</option>
                                 {kogiLga.map((lg) => <option key={lg.idlga} value={lg.name}>{lg.name}</option>)}
                             </select>
                             {errors.lga && <small className="text-red-600">{errors.lga.message}</small>}
@@ -179,7 +182,8 @@ export default function Index() {
                         </div>
                         <div className="form-group ">
                             <p>Tax Office <span className="text-red-400">*</span></p>
-                            <select name="tax_office" ref={register({ required: "Tax office is Required" })} className="form-control SlectBox mb-4 w-full rounded font-light text-gray-500">
+                            <select name="tax_office" required ref={register({ required: "Tax office is Required" })} className="form-control SlectBox mb-4 w-full rounded font-light text-gray-500">
+                                <option value="">Please Select</option>
                                 {taxOffice.map((office) => <option key={office.idstation} value={office.station_code}>{office.name}</option>)}
                             </select>
                             {errors.tax_office && <small className="text-red-600">{errors.tax_office.message}</small>}
@@ -203,7 +207,7 @@ export default function Index() {
 
                         <div className="form-group ">
                             <p>Birth Place <span className="text-red-400">*</span></p>
-                            <input name="birth_place" ref={register({ required: "Birth Place is Required" })} type="text" className="form-control mb-4 w-full rounded font-light text-gray-500"
+                            <input name="birth_place" required ref={register({ required: "Birth Place is Required" })} type="text" className="form-control mb-4 w-full rounded font-light text-gray-500"
                             />
                             {errors.birth_place && <small className="text-red-600">{errors.birth_place.message}</small>}
                         </div>
@@ -256,10 +260,10 @@ export default function Index() {
                                 {incomeSource.map((src) => <option key={src.id} value={src.source}>{src.source}</option>)}
                             </select>
                         </div>
-                        <div className="form-group col-span-2">
+                        {/* <div className="form-group col-span-2">
                             <p>Tax Authority</p>
                             <input readOnly name="tax_authority" value="Kogi State Internal Revenue Service" ref={register()} type="text" className="form-control mb-4 w-full rounded font-light text-gray-500" />
-                        </div>
+                        </div> */}
                     </div>
 
                     <div className="mb-6 flex justify-center">
