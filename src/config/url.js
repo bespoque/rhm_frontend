a4ad79ad5be3fe90ea1e96579f17c03b43390ce9
prinxard <prinxard@gmail.com>
2023-09-07 08:48:57 +0100

    update

--- src/config/url.js
+++ src/config/url.js
@@ -2,11 +2,11 @@ const url = {
 
   // live db
 
-  // BASE_URL: "https://rhmapi.bespoque.dev/api/v1/",
+  BASE_URL: "https://rhmapi.bespoque.dev/api/v1/",
 
   // Test db
 
-  BASE_URL: "https://rhmlive.bespoque.dev/api/v1/",
+  // BASE_URL: "https://rhmlive.bespoque.dev/api/v1/",
 
 };
 
