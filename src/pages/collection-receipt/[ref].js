40d09a62e2bca2318ba96eaa1f46fe18ba84d8be
prinxard <prinxard@gmail.com>
2023-09-07 09:41:32 +0100

    fixed receipt

--- src/pages/collection-receipt/[ref].js
+++ src/pages/collection-receipt/[ref].js
@@ -190,7 +190,7 @@ export default function Index() {
                             <div className="grid grid-cols-6 gap-2">
                                 <p>Details:</p>
                                 <div className="col-span-3">
-                                    <p> {el?.revenueItem} </p>
+                                    <p> {el?.details || el?.revenueItem} </p>
                                     {/* <small>{el.revenueItem}</small> */}
                                 </div>
                             </div>
