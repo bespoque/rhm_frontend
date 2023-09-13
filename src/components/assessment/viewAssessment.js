9b3a7650d4acd0d684d542442f6758d5cb604b6d
prinxard <prinxard@gmail.com>
2023-09-13 10:41:51 +0100

    update

--- src/components/assessment/viewAssessment.js
+++ src/components/assessment/viewAssessment.js
@@ -227,6 +227,14 @@ export const StartAssessment = () => {
                 <div>
                   <select required ref={register()} name="year" id="">
                     <option value="">Select Year</option>
+                    <option value="2010">2010</option>
+                    <option value="2011">2011</option>
+                    <option value="2012">2012</option>
+                    <option value="2013">2013</option>
+                    <option value="2014">2014</option>
+                    <option value="2015">2015</option>
+                    <option value="2016">2016</option>
+                    <option value="2017">2017</option>
                     <option value="2018">2018</option>
                     <option value="2019">2019</option>
                     <option value="2020">2020</option>
