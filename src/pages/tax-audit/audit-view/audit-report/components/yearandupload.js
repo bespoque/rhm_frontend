8bc4e07276181f70efccec1d57150cbb219a7599
prinxard <prinxard@gmail.com>
2023-10-27 09:57:43 +0100

    update

--- src/pages/tax-audit/audit-view/audit-report/components/yearandupload.js
+++ src/pages/tax-audit/audit-view/audit-report/components/yearandupload.js
@@ -26,41 +26,44 @@ const YearAndUpload = ({ years, selectedScope, checklistItem, onUpload }) => {
     };
 
     return (
-        <div className="flex gap-2">
-            {/* <p className="mt-4 font-bold text-center">Selected Scope: {checklistItem}</p> */}
-            <div>
-                <label htmlFor="year" className="block text-gray-700 text-sm font-bold mb-2">
-                    Select a year:
-                </label>
-                <select id="year" className="block py-2 border rounded-lg shadow-md focus:outline-none focus:ring focus:border-blue-300" onChange={handleYearChange}>
-                    <option value="">Select a year</option>
-                    {years.map((year) => (
-                        <option key={year} value={year}>
-                            {year}
-                        </option>
-                    ))}
-                </select>
+        <>
+                <p className="mt-4 font-bold text-center">Selected Scope: {checklistItem}</p>
+            <div className="flex gap-2">
+                <div>
+                    <label htmlFor="year" className="block text-gray-700 text-sm font-bold mb-2">
+                        Select a year:
+                    </label>
+                    <select id="year" className="block py-2 border rounded-lg shadow-md focus:outline-none focus:ring focus:border-blue-300" onChange={handleYearChange}>
+                        <option value="">Select a year</option>
+                        {years.map((year) => (
+                            <option key={year} value={year}>
+                                {year}
+                            </option>
+                        ))}
+                    </select>
+                </div>
+
+                <div>
+
+                    <label htmlFor="taxSchedule" className="block text-gray-700 text-sm font-bold mt-4 mb-2">
+                        Upload Tax Schedule:
+                    </label>
+                    <input type="file" id="taxSchedule" className="block px-4 py-2 border rounded-lg shadow-md focus:outline-none focus:ring focus:border-blue-300" onChange={handleTaxScheduleChange} />
+                </div>
+
+                <div>
+                    <label htmlFor="remittance" className="block text-gray-700 text-sm font-bold mt-4 mb-2">
+                        Upload Remittance:
+                    </label>
+                    <input type="file" id="remittance" className="block px-4 py-2 border rounded-lg shadow-md focus:outline-none focus:ring focus:border-blue-300" onChange={handleRemittanceChange} />
+
+                    <button onClick={handleUpload} className="mt-4 bg-blue-500 text-white rounded-lg px-4 py-2 hover:bg-blue-700">Upload</button>
+                </div>
+
+
             </div>
 
-            <div>
-
-                <label htmlFor="taxSchedule" className="block text-gray-700 text-sm font-bold mt-4 mb-2">
-                    Upload Tax Schedule:
-                </label>
-                <input type="file" id="taxSchedule" className="block px-4 py-2 border rounded-lg shadow-md focus:outline-none focus:ring focus:border-blue-300" onChange={handleTaxScheduleChange} />
-            </div>
-
-            <div>
-                <label htmlFor="remittance" className="block text-gray-700 text-sm font-bold mt-4 mb-2">
-                    Upload Remittance:
-                </label>
-                <input type="file" id="remittance" className="block px-4 py-2 border rounded-lg shadow-md focus:outline-none focus:ring focus:border-blue-300" onChange={handleRemittanceChange} />
-
-                <button onClick={handleUpload} className="mt-4 bg-blue-500 text-white rounded-lg px-4 py-2 hover:bg-blue-700">Upload</button>
-            </div>
-
-
-        </div>
+        </>
     );
 };
 
