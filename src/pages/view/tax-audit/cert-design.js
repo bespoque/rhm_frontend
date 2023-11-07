2a3f716e87a14e03989b6e48324fa2f0b867c3c3
prinxard <prinxard@gmail.com>
2023-11-07 15:25:05 +0100

    update

--- src/pages/view/tax-audit/cert-design.js
+++ src/pages/view/tax-audit/cert-design.js
@@ -32,106 +32,313 @@ const CertDesign = () => {
     };
     const formattedDateTime = new Date().toLocaleDateString(undefined, options);
 
-    function convertToNairaWords(amount) {
-        const words = [
-            "", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
-            "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen",
-            "seventeen", "eighteen", "nineteen"
-        ];
-        const tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
-        const scales = ["", "thousand", "million", "billion", "trillion", "quadrillion", "quintillion"];
+    // function convertToNairaWords(amount) {
+    //     const words = [
+    //         "", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    //         "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen",
+    //         "seventeen", "eighteen", "nineteen"
+    //     ];
+    //     const tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+    //     const scales = ["", "thousand", "million", "billion", "trillion", "quadrillion", "quintillion"];
 
-        if (amount === 0) return "zero naira";
+    //     if (amount === 0) return "zero naira";
 
-        let nairaString = "";
-        let koboString = "";
+    //     let nairaString = "";
+    //     let koboString = "";
 
-        if (amount < 0) {
-            nairaString += "minus ";
-            amount = Math.abs(amount);
+    //     if (amount < 0) {
+    //         nairaString += "minus ";
+    //         amount = Math.abs(amount);
+    //     }
+
+    //     let naira = Math.floor(amount);
+    //     let kobo = Math.round((amount - naira) * 100);
+
+    //     let scaleIndex = 0;
+    //     let lastChunkWasZero = false;
+
+    //     while (naira > 0) {
+    //         let chunk = naira % 1000;
+    //         naira = Math.floor(naira / 1000);
+    //         console.log("chunk1", chunk);
+
+    //         if (chunk > 0) {
+    //             let chunkString = "";
+    //             if (chunk < 20) {
+    //                 chunkString = words[chunk] ;
+    //                 console.log("chunk2", chunk);
+    //             } else {
+    //                 let ones = chunk % 10;
+    //                 let tensIndex = Math.floor(chunk / 10) % 10;
+    //                 let hundreds = Math.floor(chunk / 100);
+
+    //                 if (hundreds > 0) {
+    //                     chunkString += words[hundreds] + " hundred";
+    //                     if (tensIndex > 0 || ones > 0) {
+    //                         chunkString += " and " ;
+    //                     }
+    //                 }
+
+    //                 if (tensIndex > 0) {
+    //                     chunkString += tens[tensIndex];
+    //                     if (ones > 0) {
+    //                         chunkString += "-";
+    //                     }
+    //                 }
+                    
+    //                 if (ones > 0) {
+    //                     chunkString += words[ones] ;
+    //                 }
+    //             }
+                
+    //             if (scaleIndex > 0 && chunkString) {
+    //                 if (lastChunkWasZero) {
+    //                     nairaString = chunkString + " " + scales[scaleIndex] + " and " + nairaString;
+    //                 } else {
+    //                     nairaString = chunkString + " " + scales[scaleIndex] + ", " + nairaString;
+    //                 }
+    //             } else {
+    //                 nairaString = chunkString + " " + scales[scaleIndex] + " " + nairaString;
+    //             }
+    //             lastChunkWasZero = false;
+    //         } else {
+    //             lastChunkWasZero = true;
+    //         }
+            
+    //         scaleIndex++;
+    //     }
+        
+    //     if (kobo > 0) {
+    //         if (kobo < 20) {
+    //             koboString = words[kobo] + " kobo";
+    //         } else {
+    //             let ones = kobo % 10;
+    //             let tensIndex = Math.floor(kobo / 10) % 10;
+
+    //             if (tensIndex === 0) {
+    //                 koboString = words[ones] + " kobo";
+    //             } else {
+    //                 koboString = tens[tensIndex] + (ones > 0 ? "-" : "") + words[ones] + " kobo";
+    //             }
+    //         }
+    //     }
+
+    //     return nairaString.trim() + " naira " + koboString.trim() + " only";
+    // }
+
+
+    
+    function convertNumber(number) {
+        let [integer, fraction] = number.toString().split('.');
+    
+        let output = '';
+    
+        if (integer.charAt(0) === '-') {
+            output = 'negative ';
+            integer = integer.slice(1);
+        } else if (integer.charAt(0) === '+') {
+            output = 'positive ';
+            integer = integer.slice(1);
         }
-
-        let naira = Math.floor(amount);
-        let kobo = Math.round((amount - naira) * 100);
-
-        let scaleIndex = 0;
-        let lastChunkWasZero = false;
-
-        while (naira > 0) {
-            let chunk = naira % 1000;
-            naira = Math.floor(naira / 1000);
-
-            if (chunk > 0) {
-                let chunkString = "";
-                if (chunk < 20) {
-                    chunkString = words[chunk];
-                } else {
-                    let ones = chunk % 10;
-                    let tensIndex = Math.floor(chunk / 10) % 10;
-                    let hundreds = Math.floor(chunk / 100);
-
-                    if (hundreds > 0) {
-                        chunkString += words[hundreds] + " hundred";
-                        if (tensIndex > 0 || ones > 0) {
-                            chunkString += " and ";
-                        }
-                    }
-
-                    if (tensIndex > 0) {
-                        chunkString += tens[tensIndex];
-                        if (ones > 0) {
-                            chunkString += "-";
-                        }
-                    }
-
-                    if (ones > 0) {
-                        chunkString += words[ones];
-                    }
+    
+        if (integer.charAt(0) === '0') {
+            output += 'zero';
+        } else {
+            integer = integer.padStart(36, '0');
+            const group = integer.match(/.{1,3}/g).join(' ');
+            const groups = group.split(' ');
+    
+            const groups2 = groups.map(g => convertThreeDigit(g.charAt(0), g.charAt(1), g.charAt(2)))
+    
+            for (let z = 0; z < groups2.length; z++) {
+                if (groups2[z] !== '') {
+                    output += groups2[z] + convertGroup(11 - z) + (z < 11 && groups2.slice(z + 1, -1).indexOf('') === -1 && groups2[11] !== '' && groups[11].charAt(0) === '0' ? ' and ' : ', ');
                 }
-
-                if (scaleIndex > 0 && chunkString) {
-                    if (lastChunkWasZero) {
-                        nairaString = chunkString + " " + scales[scaleIndex] + " and " + nairaString;
-                    } else {
-                        nairaString = chunkString + " " + scales[scaleIndex] + ", " + nairaString;
-                    }
-                } else {
-                    nairaString = chunkString + " " + scales[scaleIndex] + " " + nairaString;
-                }
-                lastChunkWasZero = false;
-            } else {
-                lastChunkWasZero = true;
             }
-
-            scaleIndex++;
+    
+            output = output.replace(/, $/, '');
         }
-
-        if (kobo > 0) {
-            if (kobo < 20) {
-                koboString = words[kobo] + " kobo";
-            } else {
-                let ones = kobo % 10;
-                let tensIndex = Math.floor(kobo / 10) % 10;
-
-                if (tensIndex === 0) {
-                    koboString = words[ones] + " kobo";
-                } else {
-                    koboString = tens[tensIndex] + (ones > 0 ? "-" : "") + words[ones] + " kobo";
-                }
+    
+        if (fraction > 0) {
+            output += ' point';
+            for (let i = 0; i < fraction.length; i++) {
+                output += ' ' + convertDigit(fraction.charAt(i));
             }
         }
-
-        return nairaString.trim() + " naira " + koboString.trim() + " only";
+    
+        return output;
     }
-
-
-
-
-
-
-
-
-    console.log("Num", convertToNairaWords("917.90"));
+    
+    function convertGroup(index) {
+        switch (index) {
+            case 11:
+                return ' decillion';
+            case 10:
+                return ' nonillion';
+            case 9:
+                return ' octillion';
+            case 8:
+                return ' septillion';
+            case 7:
+                return ' sextillion';
+            case 6:
+                return ' quintrillion';
+            case 5:
+                return ' quadrillion';
+            case 4:
+                return ' trillion';
+            case 3:
+                return ' billion';
+            case 2:
+                return ' million';
+            case 1:
+                return ' thousand';
+            case 0:
+                return '';
+        }
+    }
+    
+    function convertThreeDigit(digit1, digit2, digit3) {
+        let buffer = '';
+    
+        if (digit1 === '0' && digit2 === '0' && digit3 === '0') {
+            return '';
+        }
+    
+        if (digit1 !== '0') {
+            buffer += convertDigit(digit1) + ' hundred';
+            if (digit2 !== '0' || digit3 !== '0') {
+                buffer += ' and ';
+            }
+        }
+    
+        if (digit2 !== '0') {
+            buffer += convertTwoDigit(digit2, digit3);
+        } else {
+            if (digit3 !== '0') {
+                buffer += convertDigit(digit3);
+            }
+        }
+    
+        return buffer;
+    }
+    
+    function convertTwoDigit(digit1, digit2) {
+        if (digit2 === '0') {
+            switch (digit1) {
+                case '1':
+                    return 'ten';
+                case '2':
+                    return 'twenty';
+                case '3':
+                    return 'thirty';
+                case '4':
+                    return 'forty';
+                case '5':
+                    return 'fifty';
+                case '6':
+                    return 'sixty';
+                case '7':
+                    return 'seventy';
+                case '8':
+                    return 'eighty';
+                case '9':
+                    return 'ninety';
+            }
+        } else {
+            if (digit1 === '1') {
+                switch (digit2) {
+                    case '1':
+                        return 'eleven';
+                    case '2':
+                        return 'twelve';
+                    case '3':
+                        return 'thirteen';
+                    case '4':
+                        return 'fourteen';
+                    case '5':
+                        return 'fifteen';
+                    case '6':
+                        return 'sixteen';
+                    case '7':
+                        return 'seventeen';
+                    case '8':
+                        return 'eighteen';
+                    case '9':
+                        return 'nineteen';
+                }
+            } else {
+                const temp = convertDigit(digit2);
+                switch (digit1) {
+                    case '2':
+                        return 'twenty-' + temp;
+                    case '3':
+                        return 'thirty-' + temp;
+                    case '4':
+                        return 'forty-' + temp;
+                    case '5':
+                        return 'fifty-' + temp;
+                    case '6':
+                        return 'sixty-' + temp;
+                    case '7':
+                        return 'seventy-' + temp;
+                    case '8':
+                        return 'eighty-' + temp;
+                    case '9':
+                        return 'ninety-' + temp;
+                }
+            }
+        }
+    }
+    
+    function convertDigit(digit) {
+        switch (digit) {
+            case '0':
+                return 'zero';
+            case '1':
+                return 'one';
+            case '2':
+                return 'two';
+            case '3':
+                return 'three';
+            case '4':
+                return 'four';
+            case '5':
+                return 'five';
+            case '6':
+                return 'six';
+            case '7':
+                return 'seven';
+            case '8':
+                return 'eight';
+            case '9':
+                return 'nine';
+        }
+    }
+    
+    // Example usage:
+    function convertToNairaAndKobo(number) {
+        const nairaAmount = Math.floor(number);
+        const koboAmount = Math.round((number - nairaAmount) * 100);
+    
+        const nairaWords = convertNumber(nairaAmount);
+        const koboWords = convertNumber(koboAmount);
+    
+        let result = nairaWords + " Naira";
+    
+        if (koboAmount > 0) {
+            result += ` and ${koboWords} Kobo`;
+        }
+    
+        return result;
+    }
+    
+    // Example usage:
+    const amount = 1234567.89;
+    const words = convertToNairaAndKobo(amount);
+    console.log(words); // Outputs "one million two hundred thirty-four thousand five hundred sixty-seven Naira and eighty-nine Kobo"
+    
+    
 
 
     useEffect(() => {
@@ -144,7 +351,7 @@ const CertDesign = () => {
         return <div>Loading...</div>;
     }
     const wordNum = (formData.amount).replace(/,/g, '')
-    const numberInWords = convertToNairaWords(wordNum);
+    const numberInWords = convertToNairaAndKobo(wordNum);
 
     return (
         <>
