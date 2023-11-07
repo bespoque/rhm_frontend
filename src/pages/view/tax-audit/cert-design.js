9c7d07776eb39785e6a87d9dec9e9015ced704e4
prinxard <prinxard@gmail.com>
2023-11-07 13:08:58 +0100

    update

--- src/pages/view/tax-audit/cert-design.js
+++ src/pages/view/tax-audit/cert-design.js
@@ -121,8 +121,9 @@ const CertDesign = () => {
             }
         }
     
-        return nairaString.trim() + " naira " + koboString.trim();
+        return nairaString.trim() + " naira " + koboString.trim() + " only";
     }
+    
     
     
     
