bf2014431904c64f461b6c39d0777940800496dc
prinxard <prinxard@gmail.com>
2023-12-08 12:09:38 +0100

    switeched to test db

--- src/config/url.js
+++ src/config/url.js
@@ -3,10 +3,10 @@ const url = {
   // live db
 
   // BASE_URL: "https://rhmapi.irs.kg.gov.ng/api/v1/",
+  // BASE_URL: "https://rhmlive.bespoque.dev/api/v1/",
 
   // Test db
-
-  BASE_URL: "https://rhmlive.bespoque.dev/api/v1/",
+BASE_URL: "https://dev.rhmapi.irs.kg.gov.ng/api/v1/",
 
 };
 
